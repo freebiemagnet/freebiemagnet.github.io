81a72a03ab898cf351677d46a6f97032c7a10ea7
Aranya Banerjee <aranyaban@yahoo.com>
2017-10-07 17:37:35 -0400

    google map

--- map.js
+++ map.js
@@ -17,12 +17,14 @@ function initMap() {
       position: location,
       label: labels[i % labels.length]
     });
+	attachSecretMessage(marker, secretMessages[i]);
   });
 
   // Add a marker clusterer to manage the markers.
   var markerCluster = new MarkerClusterer(map, markers,
       {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
 }
+var secretMessages = ['event at umbc', 'e at lib', 'the', 'secret', 'message', 'umbc baseball field', 'umbc registrars office', 'umbc department of physics'];
 var locations = [
   {lat: 39.2556, lng: -76.7110}, //umbc
   {lat: 39.2563, lng: -76.7116}, //umbc library
@@ -33,3 +35,12 @@ var locations = [
   {lat: 39.2536, lng: -76.7132}, //umbc registrars office
   {lat: 39.2545, lng: -76.7097}, //umbc department of physics
 ]
+function attachSecretMessage(marker, secretMessage) {
+  var infowindow = new google.maps.InfoWindow({
+    content: secretMessage
+  });
+
+  marker.addListener('click', function() {
+    infowindow.open(marker.get('map'), marker);
+  });
+}
